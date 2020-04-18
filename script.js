4cc87684a4c477d1011939da213c7e885bd554b7
Abhishek <ab85hishek@gmail.com>
2020-04-18 18:59:50 +0530

    version 2

--- script.js
+++ script.js
@@ -7,7 +7,37 @@ button.addEventListener("mouseenter",function(){
 )
 */
 
+var css=document.querySelector("h3");
+var color1=document.querySelector(".color1");
+var color2=document.querySelector(".color2");
+var body=document.getElementById("gradient");
 
+
+function setGradient(){
+	body.style.background=
+	"linear-gradient(to right, "
+	+color1.value
+	+", "
+	+color2.value
+	+")";
+	css.textContent = body.style.background + ";";
+
+}
+
+color1.addEventListener("input",function(){
+	setGradient();
+}
+)
+
+color2.addEventListener("input",function(){
+	setGradient();
+}
+)
+
+let d = new Date();
+document.getElementById('date').innerHTML = d.toLocaleString();
+
+//
 var button=document.getElementById("enter");
 var input=document.getElementById("userinput");
 var ul=document.querySelector("ul");
@@ -20,7 +50,8 @@ function createListElements(){
 		input.value="";
 
 		var btn=document.createElement("button");
-		btn.appendChild(document.createTextNode("Remove"));
+		btn.setAttribute("id", "del");
+		btn.appendChild(document.createTextNode("-"));
 		li.appendChild(btn);
 		btn.onclick =removeParent;
 
@@ -56,7 +87,7 @@ function listLength(){
 
 function delButton(){
 	var btn=document.createElement("button");
-	btn.appendChild(document.createTextNode("Remove"));
+	btn.appendChild(document.createTextNode("-"));
 	listItems[i].appendChild(btn);
 	btn.onclick=removeParent;
 }
@@ -69,3 +100,5 @@ for (var i=0;i<listLength();i++)
 function removeParent(evt){
 	evt.target.parentNode.remove();
 }
+
+
